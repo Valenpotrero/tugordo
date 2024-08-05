f5f3aa42147b8624b548f9e7c8ad7077b832fd7f
Valenpotrero <mendietavalen9@gmail.com>
2024-08-05 16:40:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// Función para mostrar el mensaje oculto
+document.querySelector('.heart').addEventListener('click', function() {
+    document.querySelector('.message-hidden').classList.add('show');
+    document.querySelector('.heart').classList.add('animate');
+  });
+  
+  // Animación para el corazón
